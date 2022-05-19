7b3c1fe6b7679ec4227d7eda4c981af0d93f3cb7
Matt Davidson <davidso1@rose-hulman.edu>
2022-05-19 12:16:00 -0400

    Teams icons

--- yui/src/button/js/button.js
+++ yui/src/button/js/button.js
@@ -324,7 +324,8 @@ Y.namespace('M.atto_teamsmeeting').Button = Y.Base.create('button', Y.M.editor_a
 
         if (this._currentSelection[0].collapsed) {
             // Firefox cannot add teamsmeetings when the selection is empty so we will add it manually.
-            teamsmeeting = Y.Node.create('<a>' + url + '</a>');
+            var icon = '<img src="' + this._clientdomain + '/lib/editor/atto/plugins/teamsmeeting/pix/icon.png' + '" class="icon" />';
+            teamsmeeting = Y.Node.create('<a>' + icon + 'Teams Meeting Link' + '</a>');
             teamsmeeting.setAttribute('href', url);
 
             // Add the node and select it to replicate the behaviour of execCommand.
